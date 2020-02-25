b5b3a11ace5abb55c2a526e88036320317347707
msaxon <msaxon@users.noreply.github.com>
2020-02-25 22:58:31 +0000

    Deploying to gh-pages from  - 772c9be96a41c8fbb5caa64b8b41bafe1dd4f051 🚀

---
+++ precache-manifest.606fab52cf11e473dfd310da640e3c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d538926e912acbeccbcd11b5c7704419",
+    "url": "/msaxon-dnd4ever/index.html"
+  },
+  {
+    "revision": "8f6a5db59716c6b934c2",
+    "url": "/msaxon-dnd4ever/static/css/main.951cebc0.chunk.css"
+  },
+  {
+    "revision": "3685f9e5669ebd5887df",
+    "url": "/msaxon-dnd4ever/static/js/2.6ddca9cd.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/msaxon-dnd4ever/static/js/2.6ddca9cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f6a5db59716c6b934c2",
+    "url": "/msaxon-dnd4ever/static/js/main.d266a0be.chunk.js"
+  },
+  {
+    "revision": "2503cd4c3bb43c80e588",
+    "url": "/msaxon-dnd4ever/static/js/runtime-main.1d2196fb.js"
+  }
+]);
