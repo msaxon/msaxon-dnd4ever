8c6ca8ed892af31559e541dcda0da434a2b8ccc9
msaxon <msaxon@users.noreply.github.com>
2020-04-05 20:24:09 +0000

    Deploying to gh-pages from  @ d3335c3863a22eb3a06622f7c55f4b8622050993 🚀

---
+++ precache-manifest.91e5b99c00c5f7c4101ca053678a37fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd464533cdd98790c2d8a419ad866525",
+    "url": "/msaxon-dnd4ever/index.html"
+  },
+  {
+    "revision": "f3da80d95b011fcf20a2",
+    "url": "/msaxon-dnd4ever/static/css/main.9551cb78.chunk.css"
+  },
+  {
+    "revision": "1eea0ab5ac54bda5e140",
+    "url": "/msaxon-dnd4ever/static/js/2.1751e635.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/msaxon-dnd4ever/static/js/2.1751e635.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3da80d95b011fcf20a2",
+    "url": "/msaxon-dnd4ever/static/js/main.8413d553.chunk.js"
+  },
+  {
+    "revision": "2503cd4c3bb43c80e588",
+    "url": "/msaxon-dnd4ever/static/js/runtime-main.1d2196fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/msaxon-dnd4ever/precache-manifest.6dff8cbd5942304bb66b516c0c132821.js"
+  "/msaxon-dnd4ever/precache-manifest.91e5b99c00c5f7c4101ca053678a37fd.js"
 );
 
 self.addEventListener('message', (event) => {
